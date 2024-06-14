39392e6333ae40b00c070b5a91f8ac882a34d185
H-Dohyun <dohyun033@gmail.com>
2024-06-14 10:15:07 +0900

    etc: 예시코드 분리
    Counter 컴포넌트 분리

---
+++ src/components/Counter.js
@@ -0,0 +1,43 @@
+import StateExample from "./StateExample.js";
+
+// Sample
+class Counter extends StateExample {
+  constructor(props) {
+      super(props);
+      this.render();
+  }
+
+  initState() {
+      return {
+          count: this.props.initialCount || 0
+      };
+  }
+
+  increment() {
+      this.setState({ count: this.state.count + 1 });
+  }
+
+  decrement() {
+      this.setState({ count: this.state.count - 1 });
+  }
+
+  render() {
+      const { count } = this.state;
+      const { label } = this.props;
+
+      this.container.innerHTML = `
+          <div>
+              <h3>${label}</h3>
+              <p>Count: ${count}</p>
+              <button id="increment">+</button>
+              <button id="decrement">-</button>
+          </div>
+      `;
+
+      this.container.querySelector('#increment').onclick = () => this.increment();
+      this.container.querySelector('#decrement').onclick = () => this.decrement();
+  }
+}
+
+// Usage
+const counter = new Counter({ initialCount: 5, label: 'Counter' });
--- src/components/StateExample.js
+++ src/components/StateExample.js
@@ -1,4 +1,4 @@
-class Component {
+export default class StateExample {
   constructor(props = {}) {
       this.props = props;
       this.state = {};
@@ -22,43 +22,4 @@ class Component {
   render() {}
 }
 
-class Counter extends Component {
-  constructor(props) {
-      super(props);
-      this.render();
-  }
 
-  initState() {
-      return {
-          count: this.props.initialCount || 0
-      };
-  }
-
-  increment() {
-      this.setState({ count: this.state.count + 1 });
-  }
-
-  decrement() {
-      this.setState({ count: this.state.count - 1 });
-  }
-
-  render() {
-      const { count } = this.state;
-      const { label } = this.props;
-
-      this.container.innerHTML = `
-          <div>
-              <h3>${label}</h3>
-              <p>Count: ${count}</p>
-              <button id="increment">+</button>
-              <button id="decrement">-</button>
-          </div>
-      `;
-
-      this.container.querySelector('#increment').onclick = () => this.increment();
-      this.container.querySelector('#decrement').onclick = () => this.decrement();
-  }
-}
-
-// Usage
-const counter = new Counter({ initialCount: 5, label: 'Counter' });
