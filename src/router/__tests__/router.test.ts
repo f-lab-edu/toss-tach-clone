ba3afc9f09420905b9dce5db23f835195a110c52
H-Dohyun <dohyun033@gmail.com>
2024-07-22 17:53:56 +0900

    refactor: 라우터 테스트 코드 수정
    - any 제거

--- src/router/__tests__/router.test.ts
+++ src/router/__tests__/router.test.ts
@@ -2,7 +2,7 @@ import { jest } from '@jest/globals';
 
 import router from '@/router/router';
 import { $ } from '@/utils/querySelector';
-import type { Route } from '@/types/RouterTypes';
+import type { Route, RouteParam, Router } from '@/types/RouterTypes';
 import Component from '@/core/Component';
 
 // 필요한 라우트를 설정합니다
@@ -28,14 +28,26 @@ const testRoutes: TestRoute[] = [
 	},
 ];
 
+const routeList: Route[] = testRoutes.map((route) => ({
+	path: route.path,
+	name: route.name,
+	element: route.element as unknown as new (
+		$container: HTMLElement,
+		params?: RouteParam,
+	) => Component,
+	param: route.param,
+}));
+
 describe('Router', () => {
-	(router as any).routes = testRoutes as Route[];
-	let originalPushState: (state: any, title: string, url?: string | null | undefined) => void;
+	const testRouter = router as Router;
+	testRouter.routes = routeList;
+
+	let originalPushState: (state, title: string, url?: string | null | undefined) => void;
 
 	beforeEach(() => {
 		document.body.innerHTML = '<div id="content"></div>';
 		originalPushState = window.history.pushState;
-		window.history.pushState = jest.fn((state: any, title: string, url: string) => {
+		window.history.pushState = jest.fn((state, title: string, url: string) => {
 			window.history.replaceState(state, title, url);
 		});
 	});
@@ -45,25 +57,25 @@ describe('Router', () => {
 	});
 
 	test('일치하는 경로를 찾아야 합니다', () => {
-		const route: Route = router.findMatchingRoute('/');
+		const route: Route = testRouter.findMatchingRoute('/');
 		expect(route.name).toBe('home');
 	});
 
 	test('잘못된 경로에 대해 notfound 경로를 반환해야 합니다', () => {
-		const route: Route = router.findMatchingRoute('/invalid/path');
+		const route: Route = testRouter.findMatchingRoute('/invalid/path');
 		expect(route.name).toBe('notfound');
 	});
 
 	test('경로로 이동해야 합니다', () => {
 		const contentElement: HTMLElement = $('#content');
-		router.navigateTo('/articles/1');
+		testRouter.navigateTo('/articles/1');
 		expect(window.history.pushState).toHaveBeenCalled();
 		expect(contentElement).not.toBeNull();
 	});
 
 	test('searchParams를 포함한 경로를 찾아야 합니다', () => {
 		const queryParams: URLSearchParams = new URLSearchParams({ ref: 'homepage' });
-		const route: Route = router.findMatchingRoute('/articles/1', queryParams);
+		const route: Route = testRouter.findMatchingRoute('/articles/1', queryParams);
 		expect(route.name).toBe('article');
 
 		if (route.param?.searchParams) {
@@ -76,12 +88,12 @@ describe('Router', () => {
 	});
 
 	test('searchParams를 포함한 경로로 이동해야 합니다', () => {
-		router.navigateTo('/articles/1?ref=homepage');
+		testRouter.navigateTo('/articles/1?ref=homepage');
 		expect(window.history.pushState).toHaveBeenCalled();
 	});
 
 	test('URL에 searchParams가 포함되어야 합니다', () => {
-		router.navigateTo('/articles/1?ref=homepage');
+		testRouter.navigateTo('/articles/1?ref=homepage');
 		const url: URL = new URL(window.location.href);
 		const searchParams: { [k: string]: string } = Object.fromEntries(url.searchParams.entries());
 		expect(searchParams).toEqual({ ref: 'homepage' });
