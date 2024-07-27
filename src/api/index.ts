da8301e1397aa35e6cb318be4481730b0a0220e9
H-Dohyun <dohyun033@gmail.com>
2024-07-27 11:54:21 +0900

    feat: API 랩핑 함수 추가
    콜백함수를 받아서 데이터를 반환하는 랩핑함수

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,29 +1,35 @@
+// api/index.ts
+
 import type { ArticleList, ArticleData } from '@/types/ArticleTypes';
 import ARTICLE_LIST from '@/mocks/article-list.json';
 import ARTICLE from '@/mocks/article-body.json';
 
-// async function interceptor(url: string, param?: string): Promise<T> {}
-
-async function getArticleById(): Promise<ArticleList> {
+// 콜백함수를 받아서 데이터를 반환하는 랩핑함수
+async function apiRequest<T>(callback: () => Promise<T>): Promise<T> {
 	try {
-		const data = ARTICLE_LIST;
+		const data = await callback();
 		return data;
 	} catch (error) {
-		throw new Error('Failed to fetch articles');
+		throw new Error(`Failed to fetch data: ${error instanceof Error ? error.message : error}`);
 	}
 }
 
+async function getArticleById(): Promise<ArticleList> {
+	return apiRequest(async () => {
+		const data = ARTICLE_LIST;
+		return data;
+	});
+}
+
 async function getArticle(articleId: string): Promise<ArticleData> {
-	try {
+	return apiRequest(async () => {
 		const articleContent = ARTICLE[articleId];
 		const article = ARTICLE_LIST.articles[articleId];
 		if (!articleContent || !article) {
 			throw new Error('Article not found');
 		}
 		return { articleContent, article };
-	} catch (error) {
-		throw new Error('Failed to fetch article');
-	}
+	});
 }
 
 export { getArticleById, getArticle };
