c76ac826e6e840c2e9b7d2ee667022f76ae094b1
H-Dohyun <dohyun033@gmail.com>
2024-07-28 14:57:52 +0900

    feat: 에러핸들러 추가
    API 랩핑 함수에서 에러 핸들러를 받을수 있도록 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,24 +4,34 @@ import type { ArticleList, ArticleData } from '@/types/ArticleTypes';
 import ARTICLE_LIST from '@/mocks/article-list.json';
 import ARTICLE from '@/mocks/article-body.json';
 
-// 콜백함수를 받아서 데이터를 반환하는 랩핑함수
-async function apiRequest<T>(callback: () => Promise<T>): Promise<T> {
+// 콜백함수와 에러 핸들러를 받아서 데이터를 반환하는 랩핑함수
+async function apiRequest<T>(
+	callback: () => Promise<T>,
+	errorHandler?: (error: Error) => void,
+): Promise<T> {
 	try {
 		const data = await callback();
 		return data;
 	} catch (error) {
-		throw new Error(`Failed to fetch data: ${error instanceof Error ? error.message : error}`);
+		const err = error instanceof Error ? error : new Error('Unknown error');
+		if (errorHandler) {
+			errorHandler(err);
+		}
+		throw new Error(`Failed to fetch data: ${err.message}`);
 	}
 }
 
-async function getArticleById(): Promise<ArticleList> {
+async function getArticleById(errorHandler?: (error: Error) => void): Promise<ArticleList> {
 	return apiRequest(async () => {
 		const data = ARTICLE_LIST;
 		return data;
-	});
+	}, errorHandler);
 }
 
-async function getArticle(articleId: string): Promise<ArticleData> {
+async function getArticle(
+	articleId: string,
+	errorHandler?: (error: Error) => void,
+): Promise<ArticleData> {
 	return apiRequest(async () => {
 		const articleContent = ARTICLE[articleId];
 		const article = ARTICLE_LIST.articles[articleId];
@@ -29,7 +39,7 @@ async function getArticle(articleId: string): Promise<ArticleData> {
 			throw new Error('Article not found');
 		}
 		return { articleContent, article };
-	});
+	}, errorHandler);
 }
 
 export { getArticleById, getArticle };
